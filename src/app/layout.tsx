72c448ce89c5ecc4b36a5c58cd1da717b819cc13
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2024-10-23 07:20:21 +0600

    added bootstrap layout into home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <body className={`${geistSans.variable} ${geistMono.variable}` }>
 
         <ProgressbarProvider>
             {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>User Login</h1>
-    </div>
+      <div className="container my-5">
+          <div className="row">
+              <div className="col-md-12">
+                  <h1>User Login</h1>
+              </div>
+          </div>
+      </div>
   );
 }
