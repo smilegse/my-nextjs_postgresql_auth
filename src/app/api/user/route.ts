d647b3be5d8dff2c24f6bacdf6df344961f0e91c
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2024-10-24 07:02:17 +0600

    prisma migration and generated

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -9,11 +9,11 @@ export async function POST(req: Request) {
 
         const {email, userName, password} = body;
 
-        // check if email already exists
-        const existingUserByEmail = await db.user.findUnique({ where: { email: email } });
-        if(existingUserByEmail) {
-            return NextResponse.json({user: null, message: "User with this email already exists"},{status: 409});
-        }
+        // // check if email already exists
+        // const existingUserByEmail = await db.user.findUnique({ where: { email: email } });
+        // if(existingUserByEmail) {
+        //     return NextResponse.json({user: null, message: "User with this email already exists"},{status: 409});
+        // }
 
         // check if userName already exists
         const existingUserByUserName = await db.user.findUnique({ where: { userName: userName } });
